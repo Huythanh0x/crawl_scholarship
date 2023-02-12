7be3c91b3342bb571b9227c72dcde66ac583b54f
huythanh0x <huythanh0x@gmail.com>
2023-02-12 18:33:15 +0700

    update new official website

--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ from core_class.scholars4dev import Scholars4Dev
 
 
 Scholars4Dev().main_extract_to_data_file()
-BritishCouncil().main_extract_to_data_file()
+# BritishCouncil().main_extract_to_data_file()
